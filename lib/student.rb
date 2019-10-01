6fe257902c7683429cea10e15f98a715fe3db669
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 21:58:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,8 @@ class Student
   
   def self.find_by_name(name) 
     sql = "SELECT * FROM students WHERE name = ?"
-    
+    DB[:conn].execute(sql, name).map do |row|
+      Student.new_from_db(row)
+    end
   end
 end
