fb8627b25493bf6024d9f633920489acc139ba03
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 23:54:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,8 @@ class Student
   end
   
   def update 
-    sql = "UPDATE students SET name = ?"
+    sql = "UPDATE students SET name = ?, grade = ? 
+    WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 end
